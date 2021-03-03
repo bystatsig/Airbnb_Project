83d2c2efdac4f6f6d5e3bdb80e2a5ab3a028533d
bystatsig <bystatsig@gmail.com>
2021-03-02 18:03:34 -0800

    3/2

--- Airbnb_project.py
+++ Airbnb_project.py
@@ -1,7 +1,26 @@
 """
-Purpose:
+Questions:
+Create a regression model to predict the number of reviews, or availability in the next year. Assume lack of availability is a successful
+
 
 Resources:
 """
 
+import pandas as pd
+pd.set_option('display.width',800)
+pd.set_option('display.max_columns',30)
+
+dfl = pd.read_csv('./data_files/listings.csv')
+dfc = pd.read_csv('./data_files/calendar.csv')
+dfr = pd.read_csv('./data_files/reviews.csv')
+
+df.shape
+df.describe()
+print(df.dtypes.to_string())
+df.describe()['reviews_per_month']
+
+dfr.shape
+dfr['date'].unique()
+dfc['listing_id'] =='241032'.value_counts()
+print(dfc['listing_id'].value_counts().to_string())
 
